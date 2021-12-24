8210339ba5140f33cf404593c87cc341c1d36d31
PLNA88 <paulina.gigowska@gmail.com>
2021-12-24 14:43:34 +0100

    ćwiczenie 1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
-
+        System.out.println("Tekst pisany w jednej linii.");
+        System.out.print("Tekst pisany ");
+        System.out.print("ciągiem.\n");
+        System.out.printf("Tekst, który zaciąga słowa np. %s i liczby np. %d.", "kotki", 88);
     }
 }
