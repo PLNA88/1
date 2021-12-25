f6b90838d1066a1bf7be2f50eb39d86358e8bf9e
PLNA88 <paulina.gigowska@gmail.com>
2021-12-25 20:11:00 +0100

    ćwiczenie 4

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,12 @@ public class Main {
         /* ćw.4 */
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów arytmetycznych i logicznych */
+        int p = 10, q = 20, r = 10;
+        System.out.println("Gdy p = 10, q = 20, a r = 10 operacja p*q==r*q daje rozwiązanie: "+(p*q==r*q)+".");
+        System.out.println("Natomiast operacja p+q!=q+r daje rozwiązanie: "+(p+q!=q+r)+".\n");
 
-
+        boolean a = true, b = false, c = true, d = false;
+        System.out.println("Gdy a = true, b = false, c = true, d = false operacja !(a && c)||(d||b) daje rozwiązanie: "+(!(a && c)||(d||b))+".");
+        System.out.println("Natomiast operacja a||(d||b) daje rozwiązanie: "+(a||(d||b))+".");
     }
 }
