f73b92534d339bca7c8085b16dd9c3fc7ce4ea2f
PLNA88 <paulina.gigowska@gmail.com>
2021-12-25 21:37:30 +0100

    ćwiczenie 6

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,11 @@ public class Main {
 
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
-
-
+        double[] dowolna = {885.583, 919.828, 567.765, 123.456, 191.198};
+        System.out.println(dowolna[0]);
+        System.out.println(dowolna[1]);
+        System.out.println(dowolna[2]);
+        System.out.println(dowolna[3]);
+        System.out.println(dowolna[4]);
     }
 }
